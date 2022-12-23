ecba1c266eaa3c841ae5906f2874f732fd7fb00d
Brendan Castillo <bomber8183@gmail.com>
2022-12-23 14:46:26 +0800

    added pages

---
+++ script/index/index.js
@@ -0,0 +1,11 @@
+var homebtn = document.getElementById("homebtn").addEventListener("click", function() {
+    goToPage("./homepage.html");
+});
+
+function goToPage(page) {
+    location.href = page;
+}
+
+
+
+
